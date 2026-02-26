e6bf50cc895398abc433cc0cd0bed28e7e1b6182
Gikanga-njeri <njeriteresia630@gmail.com>
2026-02-26 17:59:47 +0300

    Recommit clean changes, dependency update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,40 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+// import path from "path";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:3000", // NestJS backend port
+//         changeOrigin: true,
+//         secure: false,
+//         // rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
 
   resolve: {
     alias: {
@@ -17,12 +43,14 @@ export default defineConfig({
   },
 
   server: {
+    port: 3001,
+    strictPort: true,
+
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // NestJS backend port
+        target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
-        // rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
